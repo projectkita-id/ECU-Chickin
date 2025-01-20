3a0bbd5eff2b9ddd0f1de191e5d48a239f751710
Habib Mukhlis Triatmojo <89024106+Htech5@users.noreply.github.com>
2025-01-20 15:00:56 +0700

    Update SendREG.ino

--- Program/SendREG/SendREG.ino
+++ Program/SendREG/SendREG.ino
@@ -1,89 +1,45 @@
-/*
+#include <ModbusRtu.h>
 
-  RS485_HalfDuplex.pde - example using ModbusMaster library to communicate
-  with EPSolar LS2024B controller using a half-duplex RS485 transceiver.
+const uint8_t RE_PIN = 4;  // Receiver Enable
+const uint8_t DE_PIN = 5;  // Driver Enable
+const int LED = 13;        // LED untuk status
 
-  This example is tested against an EPSolar LS2024B solar charge controller.
-  See here for protocol specs:
-  http://www.solar-elektro.cz/data/dokumenty/1733_modbus_protocol.pdf
+// Data array untuk menyimpan register Modbus
+uint16_t modbus_array[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+// Buat objek Modbus slave
+Modbus bus(1, Serial, DE_PIN); // Slave ID = 1, Serial port, DE_PIN untuk kontrol RS485
 
-  Library:: ModbusMaster
-  Author:: Marius Kintel <marius at kintel dot net>
+void setup() {
+  pinMode(RE_PIN, OUTPUT);
+  pinMode(DE_PIN, OUTPUT);
+  pinMode(LED, OUTPUT);
 
-  Copyright:: 2009-2016 Doc Walker
+  digitalWrite(RE_PIN, LOW);  // Default ke mode Receive
+  digitalWrite(DE_PIN, LOW);  // Default ke mode Receive
 
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-
-      http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-
-*/
-
-#include <ModbusMaster.h>
-
-/*!
-  We're using a MAX485-compatible RS485 Transceiver.
-  Rx/Tx is hooked up to the hardware serial port at 'Serial'.
-  The Data Enable and Receiver Enable pins are hooked up as follows:
-*/
-const uint8_t MAX485_DE = A2;
-const uint8_t MAX485_RE_NEG = A3;
-
-// instantiate ModbusMaster object
-ModbusMaster node;
-
-void preTransmission()
-{
-  digitalWrite(MAX485_RE_NEG, 1);
-  digitalWrite(MAX485_DE, 1);
+  // Inisialisasi komunikasi Modbus
+  bus.begin(9600); // Baud rate 9600 bps
 }
 
-void postTransmission()
-{
-  digitalWrite(MAX485_RE_NEG, 0);
-  digitalWrite(MAX485_DE, 0);
-}
-
-void setup()
-{
-  pinMode(MAX485_RE_NEG, OUTPUT);
-  pinMode(MAX485_DE, OUTPUT);
-  // Init in receive mode
-  digitalWrite(MAX485_RE_NEG, 0);
-  digitalWrite(MAX485_DE, 0);
-
-  // Modbus communication runs at 115200 baud
-  Serial.begin(9600);
-
-  // Modbus slave ID 1
-  node.begin(1, Serial);
-  // Callbacks allow us to configure the RS485 transceiver correctly
-  node.preTransmission(preTransmission);
-  node.postTransmission(postTransmission);
-}
-
-
-void loop()
-{
-  uint8_t result;
-  uint16_t data[6];
-  int rpm = random(0,100);
-
-  node.writeSingleRegister(0x40001, rpm);
-  node.writeSingleRegister(0x40002, 1);
+void loop() {
+  // Simulasikan data untuk register
+  modbus_array[0] = random(0, 2);    // Register pertama (0 atau 1)
+  modbus_array[1] = 42;              // Register kedua (konstan)
+  modbus_array[2] = random(0, 100);  // Register ketiga (random 0–100)
+  modbus_array[3] = random(0, 2);    // Register pertama (0 atau 1)
+  modbus_array[4] = 42;              // Register kedua (konstan)
+  modbus_array[5] = random(0, 100);  // Register ketiga (random 0–100)
   
-  result = node.readHoldingRegisters(0x40001, 1);
-  int dataRpm = node.getResponseBuffer(0);
-  Serial.print("data : ");
-  Serial.println(node.getResponseBuffer(0));
-  if(
 
-  delay(1000);
+  // Handle polling dari master
+  bus.poll(modbus_array, sizeof(modbus_array) / sizeof(modbus_array[0]));
+
+  // Kontrol LED berdasarkan register pertama
+  if (modbus_array[6] == 1) {
+    digitalWrite(LED, HIGH); // Nyalakan LED
+  } else {
+    digitalWrite(LED, LOW);  // Matikan LED
+  }
+
+  delay(1000); // Delay 1 detik
 }
